576627be5c4c33aa8f264b2bbaf9d5d1b3eb8468
dormlaw <102824147+dormlaw@users.noreply.github.com>
2022-11-02 17:08:24 +0300

    3 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// Import stylesheets
-import './style.css';
+'use strict';
+// let promptedCode = prompt('Введите код ТН ВЭД, который хотитие проверить', '');
+// let promptedCode = new URLSearchParams(window.location.search)
+const button = document.querySelector('#checkButton');
+button.onclick = callCheck();
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function checkCode(HScode) {
+  let codeIsRestricted = false;
+  const restrictedCodes = [300610, 3006400000, 3006700000, 840732];
+
+  const exceptionCodes = [8207309000, 8407310000];
+
+  function checkIfCodeInArray(array, code = HScode) {
+    let codeInArray = false;
+    array.some((element) => {
+      element = element.toString();
+      code = code.toString().substring(0, element.length);
+      if (code.includes(element)) codeInArray = true;
+      return codeInArray;
+    });
+    return codeInArray;
+  }
+
+  if (checkIfCodeInArray(restrictedCodes)) codeIsRestricted = true;
+
+  if (codeIsRestricted) {
+    if (checkIfCodeInArray(exceptionCodes)) codeIsRestricted = false;
+  }
+
+  return codeIsRestricted;
+}
+
+function callCheck() {
+  let codeOnCheck = document.querySelector('#HScode').value;
+  let checkResult = checkCode(codeOnCheck);
+  let HTMLAnswer = document.querySelector('.answer');
+  // return console.log(checkResult, codeOnCheck);
+  if (checkResult) HTMLAnswer.innerHTML ='<p class="positive">Нужен СТ-1</p>'
+  if (!checkResult) HTMLAnswer.innerHTML ='<p class="negative">Можно отправлять</p>'
+}
