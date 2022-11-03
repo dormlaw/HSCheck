57b92586190776f2f28afc7a7da53b8f70367fd4
dormlaw <102824147+dormlaw@users.noreply.github.com>
2022-11-03 10:08:21 +0300

    add check for 3+ symbols

--- main.js
+++ main.js
@@ -10,12 +10,9 @@ function checkCode(HScode) {
       let code = codeOnCheck.toString().substring(0, element.length);
       if ( code.includes(element) ) codeInArray = true;
     });
-
     return codeInArray;
   }
-
   let codeIsRestricted = ( ! checkIfCodeInArray(restrictedCodes) || checkIfCodeInArray(exceptionCodes) ) ? false : true
-
   return codeIsRestricted;
 }
 
@@ -23,7 +20,7 @@ function callCheck() {
   let codeOnCheck = document.querySelector('#HScode').value;
   let checkResult = checkCode(codeOnCheck);
   let HTMLAnswer = document.querySelector('.answer');
-  if ( codeOnCheck !== '' ) {
+  if ( codeOnCheck !== '' && codeOnCheck.length > 3 ) {
     if ( checkResult ) HTMLAnswer.innerHTML ='<p class="positive">Нужен СТ-1</p>'
     if ( ! checkResult ) HTMLAnswer.innerHTML ='<p class="negative">Можно отправлять</p>'
   } else HTMLAnswer.innerHTML = ''
