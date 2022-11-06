fd9351461a72dd1daac515a3499903a2c314f3f7
dormlaw <dormlaw@yandex.ru>
2022-11-06 16:41:13 +0300

    restruct of funcs, codes is obj now

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 'use strict'
 
-function checkCode(HScode) {
+function checkCode(HScode, restrictedArray, exceptionArray) {
 
   function checkIfCodeInArray(array, codeOnCheck = HScode) {
     let codeInArray = false;
 
     array.some((element) => {
       element = element.toString();
-      let code = codeOnCheck.toString().substring(0, element.length);
+      let code = codeOnCheck.toString().substring(0, element.length); //обрезаем проверяемый код до длины сравниваемого, чтобы избежать ложных срабатываний
       if ( code.includes(element) ) codeInArray = true;
     });
     return codeInArray;
   }
-  let codeIsRestricted = ( ! checkIfCodeInArray(restrictedCodes) || checkIfCodeInArray(exceptionCodes) ) ? false : true
+
+  let codeIsRestricted = ( ! checkIfCodeInArray(restrictedArray) || checkIfCodeInArray(exceptionArray) ) ? false : true
+
   return codeIsRestricted;
 }
 
 function callCheck() {
   let codeOnCheck = document.querySelector('#HScode').value;
-  let checkResult = checkCode(codeOnCheck);
   let HTMLAnswer = document.querySelector('.answer');
+
+  let checkResult311 = checkCode(codeOnCheck, CODES.restricted311, CODES.exception311);
+
   if ( codeOnCheck !== '' && codeOnCheck.length > 3 ) {
-    if ( checkResult ) HTMLAnswer.innerHTML ='<p class="positive">Нужен СТ-1</p>'
-    if ( ! checkResult ) HTMLAnswer.innerHTML ='<p class="negative">Можно отправлять</p>'
+    if ( checkResult311 ) HTMLAnswer.innerHTML ='<p class="positive">Нужен СТ-1</p>'
+    if ( ! checkResult311 ) HTMLAnswer.innerHTML ='<p class="negative">Можно отправлять</p>'
   } else HTMLAnswer.innerHTML = ''
 }
